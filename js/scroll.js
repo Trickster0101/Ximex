60da2aefe078337c7130505e44d255fae1ec4543
Trickster0101 <renz.santos117@gmail.com>
2017-01-30 17:10:05 -1200

    Latest na tlga to

--- js/scroll.js
+++ js/scroll.js
@@ -1,15 +1,3 @@
-$("#aboutus").click(function() {
-  $('html, body').animate({
-    scrollTop: $("#our-company").offset().top
-  }, 2000);
-});
-
-$("#services").click(function() {
-  $('html, body').animate({
-    scrollTop: $("#our-company").offset().top
-  }, 2000);
-});
-
 $("#home").click(function() {
   $('html, body').animate({
     scrollTop: $("#top-home").offset().top
@@ -17,6 +5,18 @@ $("#home").click(function() {
 });
 
 $("#business_profile").click(function() {
+  $('html, body').animate({
+    scrollTop: $("#our-company").offset().top
+  }, 2000);
+});
+
+$("#services").click(function() {
+  $('html, body').animate({
+    scrollTop: $("#our-services").offset().top
+  }, 2000);
+});
+
+$("#aboutus").click(function() {
   $('html, body').animate({
     scrollTop: $("#our-company").offset().top
   }, 2000);
@@ -30,14 +30,24 @@ $("#contact_us").click(function() {
 
 $(window).scroll(function () {
     console.log($(window).scrollTop())
-  if ($(window).scrollTop() >= $("#our-company").offset().top) {
+  if ($(window).scrollTop() >= $("#our-company").offset().top &&
+      $(window).scrollTop() < $("#our-services").offset().top) {
     $('#nav_bar').addClass('bottom-fixed');
-    $('#myTopnav').addClass('topnav-fixed');
     $('#heading-company').addClass('heading-company-fixed');
+    $('#heading-services').addClass('heading-services-fixed');
   }
+
+  if ($(window).scrollTop() >= $("#our-services").offset().top) {
+    $('#heading-services').addClass('heading-services-fixed');
+  }
+
+  if ($(window).scrollTop() < $("#our-services").offset().top &&
+      $(window).scrollTop() > $("#our-company").offset().top) {
+    $('#heading-services').removeClass('heading-services-fixed');
+  }
+
   if ($(window).scrollTop() < $("#our-company").offset().top) {
     $('#nav_bar').removeClass('bottom-fixed');
-    $('#myTopnav').removeClass('topnav-fixed');
     $('#heading-company').removeClass('heading-company-fixed');
   }
 });

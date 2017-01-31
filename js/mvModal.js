8e58458e2292de77c528f8be21bab2878c7e44d4
Trickster0101 <renz.santos117@gmail.com>
2017-01-30 14:54:05 -1200

    Latest

--- js/mvModal.js
+++ js/mvModal.js
@@ -1,35 +1,21 @@
 // Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var missionModal = document.getElementById('myMissionModal');
+var visionModal = document.getElementById('myVisionModal');
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-function displayModal()
-{
-  modal.style.display = "block";
-}
-
-function closeModal()
-{
-  modal.style.display = "none";
-}
-
 // When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
+function openMissionModal() {
+    missionModal.style.display = "block";
+}
+
+function openVisionModal() {
+    visionModal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+function closeModal() {
+    missionModal.style.display = "none";
+    visionModal.style.display = "none";
 }

2930066918c837fec7848bf7f484d138aa7cddae
Trickster0101 <renz.santos117@gmail.com>
2017-01-31 15:14:33 -1200

    Services FIXED

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,3 +1,11 @@
+function openModal() {
+  document.getElementById('slideshow').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('slideshow').style.display = "none";
+}
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -13,6 +21,7 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -24,23 +33,4 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
-}
-
-function nextSlide() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  slideIndex++;
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace("active", "");
-  }
-
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  setTimeout(nextSlide, 5000);
 }
